7c1c3f43ab3a97d13868259b7874a66eb3557457
phamthanhtam <tampt95@gmail.com>
2021-06-08 10:14:34 +0700

    done training effect

--- src/app/training/components/current-training/current-training.component.ts
+++ src/app/training/components/current-training/current-training.component.ts
@@ -1,11 +1,7 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
-import {StopTrainingComponent} from './stop-training.component';
-import {take} from 'rxjs/operators';
-import {TrainingService} from '../../training.service';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AppState} from '../../../store';
 import {Store} from '@ngrx/store';
-import {selectTrainingProgress} from '../../store';
+import {selectTrainingProgress, stopTraining} from '../../store';
 
 @Component({
   selector: 'app-current-training',
@@ -16,26 +12,13 @@ import {selectTrainingProgress} from '../../store';
 export class CurrentTrainingComponent implements OnInit {
   progress$ = this.store.select(selectTrainingProgress);
 
-  constructor(private dialog: MatDialog,
-              private store: Store<AppState>) {
+  constructor(private store: Store<AppState>) {
   }
 
   ngOnInit(): void {
   }
 
   onStop(): void {
-    // clearInterval(this.timer);
-    // const dialogRef = this.dialog.open(StopTrainingComponent, {
-    //   data: {
-    //     progress: this.progress
-    //   }
-    // });
-    // dialogRef.afterClosed().pipe(take(1)).subscribe(result => {
-    //   if (result) {
-    //     this.exitTraining.emit();
-    //   } else {
-    //     this.doTraining();
-    //   }
-    // });
+    this.store.dispatch(stopTraining());
   }
 }
--- src/app/training/store/training.effects.ts
+++ src/app/training/store/training.effects.ts
@@ -6,8 +6,10 @@ import {AppState} from '../../store';
 import {Action, Store} from '@ngrx/store';
 import {ApiService} from '../../shared/services/api.service';
 import {of} from 'rxjs';
-import {Exercise} from '../training.model';
-import {selectCurrentTraining, selectExerciseEntities} from './training.selectors';
+import {Exercise, ExerciseEntities} from '../training.model';
+import {selectCurrentTraining, selectExerciseEntities, selectTraining} from './training.selectors';
+import {StopTrainingComponent} from '../components/current-training/stop-training.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Injectable()
 export class TrainingEffects implements OnInitEffects {
@@ -34,10 +36,7 @@ export class TrainingEffects implements OnInitEffects {
     ofType(trainingAction.startTraining),
     concatLatestFrom(() => this.store.select(selectExerciseEntities)),
     map(([action, exeEntites]) => {
-      const step = exeEntites[action.exerciseId].duration / 100 * 1000;
-      this.progressTimer = setInterval(() => {
-        this.store.dispatch(trainingAction.incrementProgress());
-      }, step);
+      this.runProcess(exeEntites, action.exerciseId);
     })
   ), {dispatch: false});
 
@@ -53,7 +52,32 @@ export class TrainingEffects implements OnInitEffects {
     })
   ), {dispatch: false});
 
+  stopTraining$ = createEffect(() => this.actions$.pipe(
+    ofType(trainingAction.stopTraining),
+    concatLatestFrom(() => this.store.select(selectCurrentTraining)),
+    map(([, currentTraining]) => {
+      clearInterval(this.progressTimer);
+      const dialogRef = this.dialog.open(StopTrainingComponent, {
+        data: {
+          progress: currentTraining.progress
+        }
+      });
+      dialogRef.afterClosed().pipe(take(1)).subscribe(result => {
+        this.store.dispatch(result ? trainingAction.finishTraining() : trainingAction.continueTraining());
+      });
+    })
+  ), {dispatch: false});
+
+  continueTraining$ = createEffect(() => this.actions$.pipe(
+    ofType(trainingAction.continueTraining),
+    concatLatestFrom(() => this.store.select(selectTraining)),
+    map(([, training]) => {
+      this.runProcess(training.exerciseEntities, training.currentTraining.exerciseId);
+    })
+  ), {dispatch: false});
+
   constructor(private actions$: Actions,
+              private dialog: MatDialog,
               private apiService: ApiService,
               private store: Store<AppState>) {
 
@@ -61,5 +85,12 @@ export class TrainingEffects implements OnInitEffects {
 
   ngrxOnInitEffects(): Action {
     return trainingAction.initData();
+  }
+
+  private runProcess(exeEntites: ExerciseEntities, exerciseId): void {
+    const step = exeEntites[exerciseId].duration / 100 * 1000;
+    this.progressTimer = setInterval(() => {
+      this.store.dispatch(trainingAction.incrementProgress());
+    }, step);
   }
 }
